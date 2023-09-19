b4c46402fc1de3cc50891ed88b11072f1b0e207e
Brandon <brandon101morgan@outlook.com>
2023-09-19 17:22:19 -0500

    Perma removed post
    Removed old app post with new app articles

--- socialbladev1/post/admin.py
+++
@@ -1,9 +0,0 @@
-from pydoc_data import topics
-from django.contrib import admin
-from post.models import Topic, Entry
-
-# Register your models here.
-
-admin.site.register(Topic)
-admin.site.register(Entry)
-
--- socialbladev1/post/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('topics/', views.topic_list, name='topic_list'),
-
-
-    ]    
